0f448d5c7db8e7c18e8e8857aa28fa2d2a959d63
ManelRuano <ruano.garcia.manel@gmail.com>
2023-12-09 14:19:31 +0100

    fin

--- app/src/main/java/com/example/threads/MainActivity.java
+++ app/src/main/java/com/example/threads/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.threads;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,60 +17,83 @@ import java.net.URL;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewData;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getDataFromUrl();
+        textViewData = findViewById(R.id.textViewData);
+        imageView = findViewById(R.id.imageView);
+
+        findViewById(R.id.buttonGetData).setOnClickListener(this::onClickGetData);
+    }
+
+    public void onClickGetData(View view) {
+        // Crida getDataFromUrl amb la URL desitjada
+        String url = "https://api.myip.com";
+        getDataFromUrl(url);
+
+        // Crida a la funció per descarregar i mostrar la imatge
+        loadAndDisplayImage();
+    }
+
+    private void getDataFromUrl(String urlString) {
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        executor.execute(() -> {
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+
+                String data = stringBuilder.toString();
+
+                // Mostra les dades a la consola de debug
+                Log.i("NetworkResponse", "Data: " + data);
+
+                // Actualitza el TextView des del fil principal
+                runOnUiThread(() -> textViewData.setText(data));
+
+            } catch (Exception e) {
+                Log.e("NetworkResponse", "Error: " + e.getMessage());
             }
         });
     }
 
-    private void getDataFromUrl() {
-        Executor executor = Executors.newSingleThreadExecutor();
-
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    // URL de ejemplo
-                    String urlString = "https://api.myip.com";
-
-                    URL url = new URL(urlString);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-
-                    // Realizar la solicitud a la URL
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                        String line;
-                        StringBuilder response = new StringBuilder();
-
-                        while ((line = reader.readLine()) != null) {
-                            response.append(line);
-                        }
-
-                        // Mostrar los datos en la consola de depuración
-                        Log.i("Datos desde la URL", response.toString());
-
-                        // Cierra la conexión
-                        connection.disconnect();
-                    } else {
-                        Log.e("Error en la solicitud", "Código de respuesta: " + responseCode);
-                    }
-                } catch (Exception e) {
-                    Log.e("Excepción", e.getMessage());
-                }
+    private void loadAndDisplayImage() {
+        new Thread(() -> {
+            String urldisplay = "https://randomfox.ca/images/122.jpg";
+            Bitmap bitmap = null;
+            try {
+                URL url = new URL(urldisplay);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
             }
-        });
+
+            // Mostra la imatge al Handler en el fil de l'UI
+            if (bitmap != null) {
+                Handler handler = new Handler(Looper.getMainLooper());
+                Bitmap finalBitmap = bitmap;
+                handler.post(() -> imageView.setImageBitmap(finalBitmap));
+            }
+        }).start();
     }
 }
